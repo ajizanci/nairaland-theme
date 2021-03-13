004fc69aa6fb96c6a5e4f9bf4cbc22f21c81190f
Favour Anefu <favouranefu@gmail.com>
2021-03-13 16:31:42 +0100

    Squashed bug that rises when categoryGroups is not set in localStorage, wrapped pages in IIFEs

--- pages/default.js
+++ pages/default.js
@@ -1,4 +1,30 @@
-(function () {
+(async function () {
+  if (!localStorage.getItem("categoryGroups")) {
+    const body = await window
+      .fetch("https://www.nairaland.com")
+      .then((r) => r.text());
+    const contents = body.match(/<div class="body">([\w\W\s]*?)<\/div>/)[1];
+    const div = document.createElement("div");
+    div.innerHTML = contents;
+    const groups = toArray(div.querySelectorAll("table.boards tr td.l")).map(
+      (td) => {
+        const links = toArray(td.querySelectorAll("a"));
+        const heading = { title: links[0].innerText, link: links[0].href };
+        return {
+          heading,
+          categories: links
+            .slice(1)
+            .map((a) => ({ title: a.innerText, link: a.href })),
+        };
+      }
+    );
+    localStorage.setItem("categoryGroups", JSON.stringify(groups));
+  }
+
+  renderDefault();
+})();
+
+function renderDefault() {
   const url = new URL(window.location.href);
   const categoryGroups = JSON.parse(localStorage.getItem("categoryGroups"));
   const categoryRoutes = categoryGroups
@@ -9,45 +35,49 @@
   );
 
   if (categoryGroup) {
-    const getThreads = safeQuery(() =>
-      toArray(
-        document.querySelectorAll("table:not([summary]) tbody tr td > b > a")
-      ).map((a) => ({ title: a.innerText, link: a.href }))
-    );
-
-    const getPagination = safeQuery(() => {
-      const p = document.querySelector("table:not([summary]) + p");
-      return [
-        {
-          page: removeBrackets(p.querySelector("b").innerText),
-          link: window.location.href,
-          current: true,
-        },
-      ]
-        .concat(
-          toArray(p.querySelectorAll("a"))
-            .filter((a) => +removeBrackets(a.innerText))
-            .map((a) => ({
-              page: removeBrackets(a.innerText),
-              link: a.href,
-            }))
-        )
-        .sort((a1, a2) => +a1.page - +a2.page);
-    });
-
-    render(
-      document.querySelector("body"),
-      Container(
-        {},
-        Header(),
-        Hero(),
-        ThreadBoard(
-          getThreads(),
-          categoryGroups,
-          getPagination(),
-          categoryGroup.title
-        )
-      )
-    );
+    renderBoard(categoryGroups, categoryGroup.title);
   }
-})();
+}
+
+function renderBoard(categoryGroups, title) {
+  const getThreads = safeQuery(() =>
+    toArray(
+      document.querySelectorAll("table:not([summary]) tbody tr td > b > a")
+    ).map((a) => ({ title: a.innerText, link: a.href }))
+  );
+
+  const getPagination = safeQuery(() => {
+    const p = document.querySelector("table:not([summary]) + p");
+    return [
+      {
+        page: removeBrackets(p.querySelector("b").innerText),
+        link: window.location.href,
+        current: true,
+      },
+    ]
+      .concat(
+        toArray(p.querySelectorAll("a"))
+          .filter((a) => +removeBrackets(a.innerText))
+          .map((a) => ({
+            page: removeBrackets(a.innerText),
+            link: a.href,
+          }))
+      )
+      .sort((a1, a2) => +a1.page - +a2.page);
+  });
+
+  render(
+    document.querySelector("body"),
+    Container(
+      {},
+      Header(),
+      Hero(),
+      ThreadBoard(
+        getThreads(),
+        categoryGroups,
+        getPagination(),
+        title
+      )
+    )
+  );
+}
--- pages/news.js
+++ pages/news.js
@@ -1,35 +1,41 @@
-const getThreads = safeQuery(() =>
-  toArray(
-    document.querySelectorAll("table[summary='links'] > tbody > tr > td a")
-  ).map((a) => ({ title: a.innerText, link: a.href }))
-);
-
-const getPagination = safeQuery(() => {
-  const p = document.querySelector(
-    "table[summary='links'] + p"
+(function () {
+  const getThreads = safeQuery(() =>
+    toArray(
+      document.querySelectorAll("table[summary='links'] > tbody > tr > td a")
+    ).map((a) => ({ title: a.innerText, link: a.href }))
   );
-  return [
-    {
-      page: removeBrackets(p.querySelector("b").innerText),
-      link: window.location.href,
-      current: true,
-    },
-  ].concat(
-    toArray(p.querySelectorAll("a"))
-      .filter((a) => +removeBrackets(a.innerText))
-      .map((a) => ({
-        page: removeBrackets(a.innerText),
-        link: a.href,
-      }))
-  ).sort((a1, a2) => +a1.page - +a2.page);
-});
 
-render(
-  document.querySelector("body"),
-  Container(
-    {},
-    Header(),
-    Hero(),
-    ThreadBoard(getThreads(), JSON.parse(localStorage.getItem("categoryGroups")) || [], getPagination())
-  )
-);
+  const getPagination = safeQuery(() => {
+    const p = document.querySelector("table[summary='links'] + p");
+    return [
+      {
+        page: removeBrackets(p.querySelector("b").innerText),
+        link: window.location.href,
+        current: true,
+      },
+    ]
+      .concat(
+        toArray(p.querySelectorAll("a"))
+          .filter((a) => +removeBrackets(a.innerText))
+          .map((a) => ({
+            page: removeBrackets(a.innerText),
+            link: a.href,
+          }))
+      )
+      .sort((a1, a2) => +a1.page - +a2.page);
+  });
+
+  render(
+    document.querySelector("body"),
+    Container(
+      {},
+      Header(),
+      Hero(),
+      ThreadBoard(
+        getThreads(),
+        JSON.parse(localStorage.getItem("categoryGroups")),
+        getPagination()
+      )
+    )
+  );
+})();
